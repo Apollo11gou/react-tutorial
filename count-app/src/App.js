e02d468e738e6990d5a2816893593131b935da28
Apollo11gou <next.soharuna@gmail.com>
2021-01-03 08:37:42 +0900

    名前の変更機能とcountapp機能の作

--- count-app/src/App.js
+++ count-app/src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
 
-import logo from "./logo.svg";
-import "./App.css";
-
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name:'にんじゃ',
+                        count:0};
+  }
+
+  handleClick(name){
+    this.setState({name:name})
+  }
+
+  countapp(){
+    this.setState({count: this.state.count +1});
+  }
+
   render() {
     return (
       <div>
-        <h1>hello React</h1>
+        <h1>こんにちは、{this.state.name}さん</h1>
+        <button
+          onClick={() => {
+            {this.handleClick('ひつじ')}
+          }}
+        >
+          ひつじ
+        </button>
+
+        <button
+          onClick={() => {
+            {this.handleClick('にんじゃわんこ')}
+          }}
+        >
+          にんじゃわんこ
+        </button>
+
+        <h1>
+          {this.state.count}
+        </h1>
+
+        <button onClick={()=>{this.countapp()}}>+</button>
       </div>
     );
   }
