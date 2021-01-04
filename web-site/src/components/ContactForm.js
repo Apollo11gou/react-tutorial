418233359456017df0b4c9f7ac601bf3f69bbfaa
Apollo11gou <next.soharuna@gmail.com>
2021-01-05 08:18:23 +0900

    エラーメッセージの表示

--- web-site/src/components/ContactForm.js
+++ web-site/src/components/ContactForm.js
@@ -1,58 +1,71 @@
-import React from 'react';
+import React from "react";
 
 class ContactForm extends React.Component {
-constructor(props) {
-  super(props);
-  this.state ={
-    isSubmitted: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitted: false,
+      // emailの入力値をstateで管理
+      email: "",
+      //入力がないときにエラーメッセージの表示
+      hasEmailError: false,
+    };
+  }
 
-    // emailの入力値をstateで管理
-    email: '',
-  };
-}
+  handleSubmit() {
+    this.setState({ isSubmitted: true });
+  }
+  handleEmailCahnge(event) {
+    const inputValue = event.target.value;
+    const isEmpty = inputValue === "";
 
-handleSubmit(){
-  this.setState({isSubmitted: true})
-}
-handleEmailCahnge(event){
-  const inputValue = event.target.value;
-
-  this.setState({email: inputValue})
-}
+    this.setState({
+      email: inputValue,
+      hasEmailError: isEmpty,
+    });
+  }
 
   render() {
+    let emailErrorText;
 
-    let  contactForm;
+    if (this.state.hasEmailError) {
+      emailErrorText = (
+        <p className="contact-message-error">
+          メールアドレスを入力してください
+        </p>
+      );
+    }
 
-    if(this.state.isSubmitted){
-      contactForm =(
-        <div className='contact-submit-message'>送信完了</div>
-      )
-    }else{
-      contactForm =(
-        <form onSubmit={()=>{this.handleSubmit()}}>
+    let contactForm;
+
+    if (this.state.isSubmitted) {
+      contactForm = <div className="contact-submit-message">送信完了</div>;
+    } else {
+      contactForm = (
+        <form
+          onSubmit={() => {
+            this.handleSubmit();
+          }}
+        >
           <p>メールアドレス(必須)</p>
           {/* inputのvalue属性でmailを表示 */}
-          <input value={this.state.email}
-          // onChangeイベントの関数でeventを関数として渡し、handleEmailCahnge()を実行
-          onChange={(event) => {this.handleEmailCahnge(event)}}
-          />
-
-          <p>お問い合わせ内容(必須)</p>
-          <textarea/>
-
           <input
-          type='submit'
-          value='送信'
+            value={this.state.email}
+            // onChangeイベントの関数でeventを関数として渡し、handleEmailCahnge()を実行
+            onChange={(event) => {
+              this.handleEmailCahnge(event);
+            }}
           />
+
+          {emailErrorText}
+          <p>お問い合わせ内容(必須)</p>
+          <textarea />
+
+          <input type="submit" value="送信" />
         </form>
-      )
+      );
     }
-    return(
-      <div className='contact-form'>
-        {contactForm}
-      </div>
-    );
+    return <div className="contact-form">{contactForm}</div>;
   }
 }
 
