b80dd7fa7db162afcb2904378a4e94a9f7171209
Apollo11gou <next.soharuna@gmail.com>
2021-01-04 11:14:10 +0900

    コンタクトフォームの作成

--- web-site/src/components/Main.js
+++ web-site/src/components/Main.js
@@ -1,29 +1,37 @@
 import React from "react";
 
 import Lesson from "./Lesson";
+import ContactForm from "./ContactForm";
 
 class Main extends React.Component {
   render() {
     const lessonList = [
       {
-        name: 'HTML & CSS',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg',
-        introduction: 'WEBページはHTML、CSSという言語によってその見た目が作られています。 実際にWEBページを作りながら学んでみましょう！',
+        name: "HTML & CSS",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg",
+        introduction:
+          "WEBページはHTML、CSSという言語によってその見た目が作られています。 実際にWEBページを作りながら学んでみましょう！",
       },
       {
-        name: 'Sass',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/sass.svg',
-        introduction: 'SassはCSSをより便利に効率的にするための言語です。',
+        name: "Sass",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/sass.svg",
+        introduction: "SassはCSSをより便利に効率的にするための言語です。",
       },
       {
-        name: 'JavaScript',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg',
-        introduction: 'JavaScriptはフロントエンドだけでなく、サーバーサイドまで広い可能性を持つプログラミング言語です。',
+        name: "JavaScript",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg",
+        introduction:
+          "JavaScriptはフロントエンドだけでなく、サーバーサイドまで広い可能性を持つプログラミング言語です。",
       },
       {
-        name: 'React',
-        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg',
-        introduction: 'ReactはHTMLのように、サイトの見た目をつくることができるJavaScriptのライブラリです。',
+        name: "React",
+        image:
+          "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg",
+        introduction:
+          "ReactはHTMLのように、サイトの見た目をつくることができるJavaScriptのライブラリです。",
       },
     ];
 
@@ -36,15 +44,20 @@ class Main extends React.Component {
           </div>
           <div className="lesson-container">
             <h3 className="sction-title">学べるレッスン</h3>
-            {lessonList.map((lessonItem)=>{
-              return(
+            {lessonList.map((lessonItem) => {
+              return (
                 <Lesson
-                name = {lessonItem.name}
-                image = {lessonItem.image}
-                introduction ={lessonItem.introduction}
+                  name={lessonItem.name}
+                  image={lessonItem.image}
+                  introduction={lessonItem.introduction}
                 />
-              )
+              );
             })}
+          </div>
+
+          <div className="contact_container">
+            <h3>お問い合わせ</h3>
+            <ContactForm />
           </div>
         </div>
       </div>
