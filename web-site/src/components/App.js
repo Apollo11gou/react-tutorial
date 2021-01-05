6b21a9a0ef551018bca2da9330e24a3644b16c5e
Apollo11gou <next.soharuna@gmail.com>
2021-01-05 09:07:10 +0900

    フッターとヘッダーの追加

--- web-site/src/components/App.js
+++ web-site/src/components/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Header from './Header';
-import Main from './Main' ;
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    return (
       <div>
         {/* <h1>hello world</h1> */}
+        <Header />
         <Main />
+        <Footer />
       </div>
     );
   }
 }
-
-
 
 export default App;
