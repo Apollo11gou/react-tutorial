7217b0daf99d138abf5ce48b28a017e014bf2d45
Apollo11gou <next.soharuna@gmail.com>
2021-01-06 21:46:27 +0900

    Dog APiからのデータ取得

--- dog-image-gallery/src/App.js
+++ dog-image-gallery/src/App.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import {fetchImages} from "./api";
+
 function Header() {
   return (
       <header className="hero is-dark is-bold">
@@ -10,37 +13,72 @@ function Header() {
       );
   }
 
-function Image() {
+function Image(props) {
+
   return(
     <div className="card">
     <div className="card-image">
     <figure className="image">
-<img
-src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-alt="cute dog"
-/>
+<img src={props.src} alt="cute dog!" />
 </figure>
     </div>
   </div>
   );
 }
 
-function Gallery() {
+function Loading() {
+  return <p>Loading...</p>;
+}
+
+function Gallery(props) {
+  const {urls} = props;
+  if (urls ===null){
+      return <Loading />;
+  }
   return(
     <div className="columns is-vcentered is-multiline">
-    <div className="column is-3">
-      <Image />
-    </div>
+      {urls.map((url)=>{
+        return(
+          // mapメソッド使用時はkey属性をつけ、繰り返される要素の間で重複しない値を割り当てる必要がある
+          <div key={url} className="column is-3">
+          {/* Imageコンポーネントの読み込み */}
+          <Image src={url}/>
+        </div>
+        );
+      })}
   </div>
   );
 }
 
+
+
 function Main() {
+  // const urls =[
+  //   "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
+  // ];
+  const [urls, setUrls] = useState(null);
+  useEffect(()=>{
+    fetchImages("shiba").then((urls) =>{
+      setUrls(urls);
+    });
+  },[]);
   return(
     <main>
     <section className="section">
       <div className="container">
-        <Gallery />
+        {/* Galleryコンポーネントの読み込み */}
+        <Gallery  urls={urls}/>
       </div>
     </section>
   </main>
