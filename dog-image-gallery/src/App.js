dd265f7b2c90c6dec8adc87ae60fbb6850e7c3c3
Apollo11gou <next.soharuna@gmail.com>
2021-01-06 16:50:17 +0900

    APP.jsの中身をコンポーネントに分割

--- dog-image-gallery/src/App.js
+++ dog-image-gallery/src/App.js
@@ -1,6 +1,5 @@
-function App() {
+function Header() {
   return (
-    <div>
       <header className="hero is-dark is-bold">
         <div className="hero-body">
           <div className="container">
@@ -8,36 +7,65 @@ function App() {
           </div>
         </div>
       </header>
+      );
+  }
 
-      <main>
-        <section className="section">
-          <div className="container">
-            <div className="columns is-vcentered is-multiline">
-              <div className="column is-3">
-                <div className="card">
-                  <div className="card-image">
-                  <figure className="image">
-          <img
-            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-            alt="cute dog"
-          />
-        </figure>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+function Image() {
+  return(
+    <div className="card">
+    <div className="card-image">
+    <figure className="image">
+<img
+src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+alt="cute dog"
+/>
+</figure>
+    </div>
+  </div>
+  );
+}
 
-      <footer className="footer">
-        <div className="content has-text-centered">
-        <p>Dog images are retrieved from Dog API</p>
-        <p>
-          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
-        </p>
-        </div>
-      </footer>
+function Gallery() {
+  return(
+    <div className="columns is-vcentered is-multiline">
+    <div className="column is-3">
+      <Image />
+    </div>
+  </div>
+  );
+}
+
+function Main() {
+  return(
+    <main>
+    <section className="section">
+      <div className="container">
+        <Gallery />
+      </div>
+    </section>
+  </main>
+  );
+}
+
+function Footer() {
+  return(
+    <footer className="footer">
+    <div className="content has-text-centered">
+    <p>Dog images are retrieved from Dog API</p>
+    <p>
+      <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+    </p>
+    </div>
+  </footer>
+  );
+}
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
