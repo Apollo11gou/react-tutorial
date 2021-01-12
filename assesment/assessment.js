e5b8c1d368c06ede66ab2b2b50b22edace783b56
Apollo11gou <next.soharuna@gmail.com>
2021-01-12 10:40:26 +0900

    診断結果が連続で出ないようにする関数の追加

--- assesment/assessment.js
+++ assesment/assessment.js
@@ -4,12 +4,26 @@ const assessmentButton = document.getElementById("assessment");
 const resultDivided = document.getElementById("result-area");
 const tweetDivided = document.getElementById("tweet-area");
 
+/**
+ * 指定した要素の子どもを全て削除する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    // 子どもの要素があるかぎり削除
+    element.removeChild(element.firstChild);
+  }
+}
+
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
   if (userName.length === 0) {
     // 名前が空のときは処理を終了する
     return;
   }
+
+  //診断結果表示エリアの作成
+  removeAllChildren(resultDivided);
   const header = document.createElement("h3");
   header.innerText = "診断結果";
   resultDivided.appendChild(header);
